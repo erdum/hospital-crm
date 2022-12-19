3b83f40be2f14974717a0486a18fdf1c956448ce
Erdum <erdumadnan@gmail.com>
2022-12-19 14:55:53 +0500

    Update Department model relationship established

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Physician;
 
 class Department extends Model
 {
     use HasFactory;
+
+    public function head()
+    {
+        return $this->belongsTo(Physician::class, 'head_id');
+    }
 }
