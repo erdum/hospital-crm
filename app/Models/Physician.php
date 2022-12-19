5d3deb71ab6ffffaa06341bd119c045ede401855
Erdum <erdumadnan@gmail.com>
2022-12-19 13:38:31 +0500

    Update Physician model relationship added

--- app/Models/Physician.php
+++ app/Models/Physician.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Appointment;
 
 class Physician extends Model
 {
     use HasFactory;
+
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class);
+    }
 }
