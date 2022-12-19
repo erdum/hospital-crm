81ca2c9b9e5c6c45bcce50ec23d07b457d6c45b1
Erdum <erdumadnan@gmail.com>
2022-12-19 14:54:40 +0500

    Update Procedures model relationships established

--- app/Models/Procedure.php
+++ app/Models/Procedure.php
@@ -4,8 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Patient;
+use App\Models\Physician;
+use App\Models\Nurse;
+use App\Models\Stay;
+use App\Models\Department;
 
 class Procedure extends Model
 {
     use HasFactory;
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function physician()
+    {
+        return $this->belongsTo(Physician::class);
+    }
+
+    public function nurse()
+    {
+        return $this->belongsTo(Nurse::class);
+    }
+
+    public function stay()
+    {
+        return $this->belongsTo(Stay::class);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
