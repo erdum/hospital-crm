a0eecfbf6cf2e63fdc9a1238d6616d53873668b6
Erdum <erdumadnan@gmail.com>
2022-12-19 14:55:02 +0500

    Update Prescribes model relationships established

--- app/Models/Prescribes.php
+++ app/Models/Prescribes.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Physician;
+use App\Models\Patient;
+use App\Models\Medication;
 
 class Prescribes extends Model
 {
     use HasFactory;
+
+    public function physician()
+    {
+        return $this->belongsTo(Physician::class);
+    }
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function medication()
+    {
+        return $this->belongsTo(Medication::class);
+    }
 }
