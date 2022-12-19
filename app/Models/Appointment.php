b4506af533e50cfe94e44d1608b1f3310e2e78d4
Erdum <erdumadnan@gmail.com>
2022-12-19 12:35:55 +0500

    Update Appointments relationships defined

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Patient;
+use App\Models\Physician;
+use App\Models\Room;
 
 class Appointment extends Model
 {
     use HasFactory;
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function physician()
+    {
+        return $this->belongsTo(Physician::class);
+    }
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
 }
