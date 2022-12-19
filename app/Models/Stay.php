fdcca0eab00a7cef25f8ab24318aae7392537399
Erdum <erdumadnan@gmail.com>
2022-12-19 13:04:37 +0500

    Update Stay model relationships established

--- app/Models/Stay.php
+++ app/Models/Stay.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Room;
+use App\Models\Patient;
 
 class Stay extends Model
 {
     use HasFactory;
+
+    public function room(){
+        return $this->belongsTo(Room::class);
+    }
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
 }
