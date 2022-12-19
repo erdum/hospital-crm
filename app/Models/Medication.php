7720b3928783357da39a5d25ffa6809e9e1c0f60
Erdum <erdumadnan@gmail.com>
2022-12-19 14:55:22 +0500

    Update Medication model relationships established

--- app/Models/Medication.php
+++ app/Models/Medication.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Prescribes;
 
 class Medication extends Model
 {
     use HasFactory;
+
+    public function prescribes()
+    {
+        return $this->belongsTo(Prescribes::class);
+    }
 }
